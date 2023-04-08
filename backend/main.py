a74184558ba30664f0fc10e1db14b30ba87a3f65
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2023-04-07 20:23:10 -0400

    base files

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 from alpaca.trading.client import TradingClient
+from config import *
 
-API_KEY = "PKGK3ZI8BEG3QD3JCJLL"
-SECRET_KEY = "XXiJ5gSrOG4v0LuOADi9gquNElFEkSkepmyacG0v"
-
-trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+trading_client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
 account = trading_client.get_account()
 
