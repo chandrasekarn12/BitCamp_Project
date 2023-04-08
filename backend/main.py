c3178f18492dd912e6cba7b759b0eb867499714d
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2023-04-07 20:34:50 -0400

    moo

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,3 @@
 import config
-from alpaca.trading.client import TradingClient
 
-
-trading_client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
-account = trading_client.get_account()
 
--- backend/util.py
+++ backend/util.py
@@ -1,6 +1,11 @@
-import main
+import config
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.client import TradingClient
+
+
+trading_client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
+account = trading_client.get_account()
 
 
 def order(symbol, qty, side):
@@ -12,7 +17,7 @@ def order(symbol, qty, side):
             time_in_force=TimeInForce.GTC
         )
 
-        main.trading_client.submit_order(market_order_data)
+        trading_client.submit_order(market_order_data)
         return True
 
     except Exception as e:
